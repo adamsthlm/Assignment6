bee508557e6276080bdeb1f868aa05043b050a44
Carl-Adam Berglund <adam@carl-adam.tech>
2020-05-21 18:17:19 +0200

    Live time and date in place at lower right corner, timer1 set at 100

---
+++ MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Assignment6
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newCtrlNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.openDatafileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDatafileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.omToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblLiveClock = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LabelDate = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCtrlNToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.openDatafileToolStripMenuItem,
+            this.saveDatafileToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitAltF4ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newCtrlNToolStripMenuItem
+            // 
+            this.newCtrlNToolStripMenuItem.Name = "newCtrlNToolStripMenuItem";
+            this.newCtrlNToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.newCtrlNToolStripMenuItem.Text = "New      Ctrl + N";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(155, 6);
+            // 
+            // openDatafileToolStripMenuItem
+            // 
+            this.openDatafileToolStripMenuItem.Name = "openDatafileToolStripMenuItem";
+            this.openDatafileToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.openDatafileToolStripMenuItem.Text = "Open datafile";
+            // 
+            // saveDatafileToolStripMenuItem
+            // 
+            this.saveDatafileToolStripMenuItem.Name = "saveDatafileToolStripMenuItem";
+            this.saveDatafileToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.saveDatafileToolStripMenuItem.Text = "Save datafile";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(155, 6);
+            // 
+            // exitAltF4ToolStripMenuItem
+            // 
+            this.exitAltF4ToolStripMenuItem.Name = "exitAltF4ToolStripMenuItem";
+            this.exitAltF4ToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.exitAltF4ToolStripMenuItem.Text = "Exit Alt +F4";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.omToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // omToolStripMenuItem
+            // 
+            this.omToolStripMenuItem.Name = "omToolStripMenuItem";
+            this.omToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.omToolStripMenuItem.Text = "Om...";
+            // 
+            // lblLiveClock
+            // 
+            this.lblLiveClock.AutoSize = true;
+            this.lblLiveClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLiveClock.Location = new System.Drawing.Point(659, 407);
+            this.lblLiveClock.Name = "lblLiveClock";
+            this.lblLiveClock.Size = new System.Drawing.Size(110, 25);
+            this.lblLiveClock.TabIndex = 1;
+            this.lblLiveClock.Text = "HH:mm:ss";
+            this.lblLiveClock.Click += new System.EventHandler(this.LabelLiveClock_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // LabelDate
+            // 
+            this.LabelDate.AutoSize = true;
+            this.LabelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelDate.Location = new System.Drawing.Point(636, 382);
+            this.LabelDate.Name = "LabelDate";
+            this.LabelDate.Size = new System.Drawing.Size(152, 25);
+            this.LabelDate.TabIndex = 2;
+            this.LabelDate.Text = "YYYY-MM-DD";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LabelDate);
+            this.Controls.Add(this.lblLiveClock);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainForm";
+            this.Text = "Carl-Adam Technologies - ToDo Reminder 0.1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newCtrlNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openDatafileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveDatafileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitAltF4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem omToolStripMenuItem;
+        private System.Windows.Forms.Label lblLiveClock;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label LabelDate;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Assignment6
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblLiveClock.Text = DateTime.Now.ToString("HH:mm:ss");
+            LabelDate.Text = DateTime.Now.ToString("yyyy-MM-dd");
+
+            
+        }
+
+        private void MainForm_Load(Object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void LabelLiveClock_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
